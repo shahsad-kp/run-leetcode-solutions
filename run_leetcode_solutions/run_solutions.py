c28ecc96dead522c2bd1cee248f0972be0c693d6
Shahsad KP <shahsadkpklr@gmail.com>
2025-08-17 08:14:31 +0530

    Refactor load_solution_class to accept a customizable folder and remove unused argument parsing code

--- run_leetcode_solutions/run_solutions.py
+++ run_leetcode_solutions/run_solutions.py
@@ -1,16 +1,11 @@
-import argparse
-import ast
 import importlib.util
 import os
-import sys
-
-from base_solution import BaseSolution
 
 
-def load_solution_class(file_number, folder):
-    file_name = os.path.join("questions", f"{file_number}.py")
+def load_solution_class(file_number, folder = "questions"):
+    file_name = os.path.join(folder, f"{file_number}.py")
     if not os.path.exists(file_name):
-        raise FileNotFoundError(f"{file_name} not found in 'questions/' directory.")
+        raise FileNotFoundError(f"{file_name} not found in '{folder}/' directory.")
 
     spec = importlib.util.spec_from_file_location("solution_module", file_name)
     module = importlib.util.module_from_spec(spec)
@@ -32,38 +27,3 @@ def get_method(solution):
         raise Exception(f"Method '{method_name}' not found or not callable in Solution class.")
 
     return method_name, method
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run solution interactively or with test cases.")
-    parser.add_argument("--question", type=str, help="Question number (e.g., 1 for 1.py)")
-    parser.add_argument("--mode", type=str, choices=["a", "m"], help="Run mode: a (auto) or m (manual)")
-    parser.add_argument("--inputs", type=str, help="Manual inputs as Python list string (e.g., \"[1, 2]\")")
-    return parser.parse_args()
-
-
-def run_solutions(questions_folder: str = 'questions'):
-    args = parse_args()
-
-    question_number = args.question or input("Enter question number: ").strip()
-    mode = args.mode or input("Run in (A)utomatic or (M)anual mode? ").strip().lower()
-    inputs_from_cli = None
-
-    if args.inputs:
-        try:
-            inputs_from_cli = ast.literal_eval(args.inputs)
-            if not isinstance(inputs_from_cli, list):
-                raise ValueError("Inputs must be a list.")
-        except Exception as e:
-            print(f"Error parsing inputs: {e}")
-            sys.exit(1)
-
-    solution: BaseSolution = load_solution_class(question_number, questions_folder)
-    print(f"\nQuestion: {getattr(solution, 'title', 'No question description found.')}\n")
-
-    if mode == "a":
-        solution.run_automatic()
-    elif mode == "m":
-        solution.run_manual(inputs_from_cli)
-    else:
-        print("Invalid mode. Choose 'a' or 'm'.")
